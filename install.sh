19958aebd6df5d42f4549729c23ffe6e5e289c7a
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-23 21:56:22 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -43,13 +43,10 @@ setup_certificate() {
 install_ntfy() {
   # Create a directory for apt keyrings
   sudo mkdir -p /etc/apt/keyrings
-
   # Download and add the GPG key for the Heckel repository
   curl -fsSL https://archive.heckel.io/apt/pubkey.txt | sudo gpg --dearmor -o /etc/apt/keyrings/archive.heckel.io.gpg
-
   # Install the apt-transport-https package
   sudo apt install apt-transport-https
-
   # Add the Heckel repository to sources.list.d
   sudo sh -c "echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/archive.heckel.io.gpg] https://archive.heckel.io/apt debian main' \
   > /etc/apt/sources.list.d/archive.heckel.io.list"
