428ad8bfab935deb9d162187938381a51b82b2bf
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-24 22:59:39 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -244,7 +244,7 @@ uninstall_ntfy_docker() {
 
 # Main menu
 clear
-echo "By --> Peyman * Github.com/Ptechgithub * "
+echo "By --.> Peyman * Github.com/Ptechgithub * "
 echo "** NTFY Installer--> PUSH NOTIFICATION**"
 echo ""
 echo "Select an option:"
