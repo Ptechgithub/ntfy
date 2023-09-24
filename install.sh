643f45da28f8006bec1dcaa3b5a845fadfdd15ed
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-24 21:56:59 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -211,7 +211,7 @@ uninstall_ntfy_docker() {
     rm -rf /etc/ntfy
     echo "ntfy Docker container has been uninstalled."
   else
-    echo "ntfy Docker container is not running or does not exist."
+    echo "ntfy Docker container is not running or does not install."
   fi
 }
 
