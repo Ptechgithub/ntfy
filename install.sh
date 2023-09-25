c989e666a96b5d7c1f814bce42e1564f6e3ff5eb
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-25 16:07:45 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -264,7 +264,7 @@ apply() {
 
 # Main menu
 clear
-echo "By --> Peyman * Github.com/Ptechgithub * "
+echo "By -2-> Peyman * Github.com/Ptechgithub * "
 echo "** NTFY Installer--> PUSH NOTIFICATION**"
 echo ""
 echo "Select an option:"
